1a12d90a8e4f019fa218f26ea17d225adbc763ab
godfried <godfried@bungle.spiderland>
2012-07-28 18:29:34 +0200

    removed old version

--- src/com/cs/shopper/client/rpc/ClassService.java
+++
@@ -1,9 +0,0 @@
-package com.cs.shopper.client.rpc;
-
-import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-@RemoteServiceRelativePath("class")
-public interface ClassService  extends RemoteService {
-
-	boolean registerClasses();
-}
